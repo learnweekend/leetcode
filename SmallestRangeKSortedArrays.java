6ccffa4381394c0d7f843ab88a6f5b525d0b3d59
learnweekend <learnweekend@gmail.com>
2018-12-12 21:31:02 -0800

    Update SmallestRangeKSortedArrays.java

--- SmallestRangeKSortedArrays.java
+++ SmallestRangeKSortedArrays.java
@@ -1,130 +1,83 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 /**
  * https://www.youtube.com/watch?v=zplklOy7ENo&list=PLA8lYuzFlBqAy6dkZHj5VxUAaqr4vwrka&index=3
- * 
  * https://leetcode.com/problems/smallest-range/
- * 
- * Input:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
-Output: [20,24]
-Explanation: 
-List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
-List 2: [0, 9, 12, 20], 20 is in range [20,24].
-List 3: [5, 18, 22, 30], 22 is in range [20,24]
-
-	//TODO NOT COMPLETE
- * 
- *
+ * Input:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]] Output: [20,24] Explanation: List 1: [4, 10,
+ * 15, 24,26], 24 is in range [20,24]. List 2: [0, 9, 12, 20], 20 is in range [20,24]. List 3: [5,
+ * 18, 22, 30], 22 is in range [20,24]
  */
 public class SmallestRangeKSortedArrays {
+	
+	public static int[] smallestRange(List<List<Integer>> sortedLists) {
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int range = Integer.MAX_VALUE;
+		int minRange = Integer.MAX_VALUE;
+		int minRangeMin = 0;
+		int minRangeMax = 0;
+		
+		PriorityQueue<QueueNode> pq = new PriorityQueue<QueueNode>();
+	// Insert first element from each list
+		for (int i = 0; i < sortedLists.size(); i++) {
+			if (sortedLists.get(i).size() > 0) {
+				pq.add(new QueueNode(i, 0, sortedLists.get(i).get(0)));
+				// calculate the max for first elements added to queue
+				max = Math.max(max, sortedLists.get(i).get(0)); 
+			}
+		}
+		while (!pq.isEmpty()) {
+			QueueNode node = pq.poll(); // poll the minimum node from pq
+			min = node.value;
+			range = max - min;   // calculate the range
+			if(range < minRange) {  // check if range is < minRange
+				minRange = range;
+				minRangeMin = min;
+				minRangeMax = max;
+			}
+			int newIndex = node.index + 1;  // next element to add into PQ
+			if (newIndex < sortedLists.get(node.list).size()) {  // check if any of the list falls out of range.
+				pq.add(new QueueNode(node.list, newIndex, sortedLists.get(node.list).get(newIndex)));
+				max = Math.max(max, sortedLists.get(node.list).get(newIndex));
+			} else {
+				return new int[] { minRangeMin, minRangeMax };
+			}
+		}
+		return new int[] { minRangeMin, minRangeMax };
+	}
+
+	private static class QueueNode implements Comparable<QueueNode> {
+		private int list;
+		private int index;
+		private int value;
+
+		public QueueNode(int list, int index, int value) {
+			this.list = list;
+			this.index = index;
+			this.value = value;
+		}
+
+		public int compareTo(QueueNode n) {
+			if (value > n.value)
+				return 1;
+			if (value < n.value)
+				return -1;
+			return 0;
+		}
+	}
 
 	public static void main(String[] args) {
 		List<List<Integer>> lists = new ArrayList<>();
-		List<Integer> list1 =  Arrays.asList(4,10,15,24,26);
-		List<Integer> list2 =  Arrays.asList(0,9,12,20);
-		List<Integer> list3 =  Arrays.asList(5,18,22,30);
-		
+		List<Integer> list1 = Arrays.asList(4, 10, 15, 24, 26);
+		List<Integer> list2 = Arrays.asList(0, 9, 12, 20);
+		List<Integer> list3 = Arrays.asList(5, 18, 22, 30);
 		lists.add(list1);
 		lists.add(list2);
 		lists.add(list3);
-		
 		int[] range = smallestRange(lists);
 		System.out.println(Arrays.toString(range));
 	}
-	
-	public static int[] smallestRange(List<List<Integer>> sortedList) {
-		
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		int range = 0;
-		
-		int minRange = Integer.MAX_VALUE;
-		int minRangeMin = Integer.MAX_VALUE;
-		int minRangeMax = Integer.MIN_VALUE;
-		
-		PriorityQueue<List<Integer>> pQueue = 
-				new PriorityQueue<>(new Comparator<List<Integer>>(){
-			@Override
-			public int compare(List<Integer> node1, List<Integer> node2) {
-				return node1.get(0) - node2.get(0);
-			}
-		});
-		
-		int index = 0;
-		for(int i = 0; i < sortedList.size(); i++) {
-			List<Integer> list = sortedList.get(0);
-			int val = list.get(0);
-			if(val < min) {
-				min = val;
-				index = i;
-			} 
-			if(val > max) {
-				max = val;
-			}
-		}
-		range = max - min;
-		
-		System.out.println(min);
-		System.out.println(max);
-		System.out.println(range);
-		
-		if(range < minRange) {
-			minRange = range;
-			minRangeMin = min;
-			minRangeMax = max;
-		}
-		
-		for(int i = 0; i < sortedList.size(); i++) {
-			List<Integer> list = sortedList.get(i);
-			List<Integer> temp = new ArrayList<>();
-			if(index == i) {
-				temp.addAll(list);
-				temp.remove(0);
-				if(temp != null && temp.size() > 0) {
-					pQueue.add(temp);
-				}
-			}
-			if(list != null && list.size() > 0) {
-				pQueue.add(list);
-			}
-		}
-		
-		for(List<Integer> list : sortedList) {
-			List<Integer> temp = new ArrayList<>();
-			temp.addAll(list);
-			temp.remove(0);
-			if(temp != null && temp.size() > 0) {
-				pQueue.add(temp);
-			}
-		}
-		
-		while (!pQueue.isEmpty()) {
-			System.out.println();
-			List<Integer> node = pQueue.poll();
-			min = node.get(0);
-			node.remove(0);
-			int temp = Integer.MIN_VALUE;
-			if(node.size() > 0) {
-				temp = node.get(0);
-				pQueue.add(node);
-				if(temp > max) {
-					max = temp;
-				}
-				range = max - min;
-				if(range < minRange) {
-					minRange = range;
-					minRangeMin = min;
-					minRangeMax = max;
-				}
-			} else {
-				return new int[] {minRangeMin, minRangeMax};
-			}
-		}
-		return new int[] {minRangeMin, minRangeMax};
-	}
-
 }
