111aa5d7b6c7f5d28f7af36aa84b2c885edaf536
learnweekend <learnweekend@gmail.com>
2018-12-28 15:07:33 -0800

    update count palindromic substrings

--- PalindromicSubStrings.java
+++ PalindromicSubStrings.java
@@ -1,24 +1,43 @@
 
+/** https://leetcode.com/problems/palindromic-substrings/description/
+ * 
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ * 
+ * The substrings with different start indexes or end indexes are counted as different substrings
+ * even they consist of same characters.
+ * 
+ * Example 1:
+ * 
+ * Input: "abc" Output: 3 Explanation: Three palindromic strings: "a", "b", "c".
+ * 
+ * Example 2:
+ * 
+ * Input: "aaa" Output: 6 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ */
 public class PalindromicSubStrings {
 
-	public static void main(String[] args) {
-		String s = "abcde";
-		System.out.println(countSubstrings(s));
-	}
-
-	public static int countSubstrings(String S) {
-		int N = S.length(), ans = 0;
-		for (int center = 0; center <= 2 * N - 1; ++center) {
-			int left = center / 2;
-			int right = left + center % 2;
-			
-			while (left >= 0 && right < N ) {
-				ans++;
-				left--;
-				right++;
-			}
+	public static int countSubstrings(String s) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int even = countSubstrings(s, i, i);
+			int odd = countSubstrings(s, i, i + 1);
+			count += (even + odd);
 		}
-		return ans;
+		return count;
 	}
 
+	private static int countSubstrings(String s, int left, int right) {
+		int count = 0;
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			count++;
+			left--;
+			right++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countSubstrings("abc"));
+		System.out.println(countSubstrings("aaa"));
+	}
 }
