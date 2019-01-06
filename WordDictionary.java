aebdc8acde9c6d660e0696c7bb328af6b6bf264d
learnweekend <learnweekend@gmail.com>
2019-01-06 14:11:25 -0800

    Update WordDictionary.java

--- WordDictionary.java
+++ WordDictionary.java
@@ -1,19 +1,26 @@
+package leetcode;
+
 import java.util.HashMap;
 import java.util.Map;
 
+/**'https://leetcode.com/problems/add-and-search-word-data-structure-design/description/
+ * 
+ * Design a data structure that supports the following two operations:
+ * 
+ * void addWord(word) bool search(word)
+ * 
+ * search(word) can search a literal word or a regular expression string containing only letters a-z
+ * or .. A . means it can represent any one letter.
+ * 
+ * Example:
+ * 
+ * addWord("bad") addWord("dad") addWord("mad") search("pad") -> false search("bad") -> true
+ * search(".ad") -> true search("b..") -> true
+ * 
+ * Note: You may assume that all words are consist of lowercase letters a-z.
+ */
 class WordDictionary {
 	
-	public static void main(String[] args) {
-		WordDictionary dictionary = new WordDictionary();
-		dictionary.addWord("bad");
-		dictionary.addWord("dad");
-		dictionary.addWord("mad");
-		System.out.println(dictionary.search("pad")); //-> false
-		System.out.println(dictionary.search("bad")); //-> true
-		System.out.println(dictionary.search(".ad")); // -> true
-		System.out.println(dictionary.search("b..")); // -> true
-	}
-
 	private TrieNode root;
 	
    /** Initialize your data structure here. */
@@ -77,4 +84,15 @@ class WordDictionary {
    			this.isWord = false;
    		}
    }
+   
+   public static void main(String[] args) {
+		WordDictionary dictionary = new WordDictionary();
+		dictionary.addWord("bad");
+		dictionary.addWord("dad");
+		dictionary.addWord("mad");
+		System.out.println(dictionary.search("pad")); //-> false
+		System.out.println(dictionary.search("bad")); //-> true
+		System.out.println(dictionary.search(".ad")); // -> true
+		System.out.println(dictionary.search("b..")); // -> true
+	}
 }
