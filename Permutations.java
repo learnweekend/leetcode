ea8f5d12c3354d95af2f3176dbad23d5ef8b6089
learnweekend <learnweekend@gmail.com>
2019-09-29 12:01:05 -0700

    Add files via upload

--- Permutations.java
+++ Permutations.java
@@ -1,54 +1,91 @@
+package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * https://leetcode.com/problems/permutations/description/
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+ */
 public class Permutations {
-	
-	private static int count = 0;
 
-	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3};
-		List<List<Integer>> result = permute(arr);
-		for(List<Integer> p : result) {
-			System.out.println(p);
+	// Runtime : O(n * n!)  |  space : O(n * n!)
+	public static List<List<Integer>> getPermutations(ArrayList<Integer> array) {
+		List<List<Integer>> permutations = new ArrayList<>();
+		
+		if (array == null || array.size() == 0)
+			return permutations;
+
+		permutations(array, permutations, 0);
+		return permutations;
+	}
+
+	private static void permutations(List<Integer> array, final List<List<Integer>> permutations, int index) {
+		if (index == array.size()) {
+			ArrayList<Integer> permute = new ArrayList<>(array);
+			permutations.add(permute);
+			return;
+		}
+		Set<Integer> set = new HashSet<>(); // to handle duplicates
+		for (int i = index; i < array.size(); i++) {
+			if (set.add(array.get(i))) {   // to handle duplicate values in input array
+				swap(array, index, i);
+				permutations(array, permutations, index + 1);
+				swap(array, index, i);
+			}
 		}
 	}
-	
-	public static List<List<Integer>> permute(int[] nums) {
-      List<List<Integer>> permutations = new ArrayList<>();
-      if(nums == null || nums.length == 0)
-         return permutations;
-      boolean[] isInBuffer = new boolean[nums.length];
-      int[] buffer = new int[nums.length];
-      permute(permutations, nums, buffer, 0, isInBuffer);
-      return permutations;
-  }
-  
-  public static void permute(
-      List<List<Integer>> permutations, 
-      int[] arr, 
-      int[] buffer, 
-      int bufferIndex, 
-      boolean[] isInBuffer) {
-      
-      //base case
-      if(bufferIndex == buffer.length){
-          List<Integer> permute = new ArrayList<Integer>();
-          for (int i : buffer)
-         	 	permute.add(i);
-          permutations.add(permute);
-          return;
-      }
-      // find next candidate to buffer
-      for(int i = 0; i < arr.length; i++) {
-      		System.out.println(count++);
-          if(!isInBuffer[i]){
-              buffer[bufferIndex] = arr[i];  // fill the buffer
-              isInBuffer[i] = true;
-              permute(permutations, arr, buffer, bufferIndex + 1, isInBuffer); // recursive
-              isInBuffer[i] = false; 
-          }
-      }
-  }
 
+	public static List<String> stringPermutations(String str) {
+		if (str == null || str.length() == 0)
+			throw new IllegalArgumentException();
+		
+		List<String> permutations = new ArrayList<String>();
+		stringPermutations("", str, permutations);
+		return permutations;
+	}
+
+	private static void stringPermutations(String prefix, String suffix, List<String> permutations) {
+		if (suffix.length() == 0)
+			permutations.add(prefix);
+		else {
+			for (int i = 0; i < suffix.length(); i++) {
+				stringPermutations(prefix + suffix.charAt(i), suffix.substring(0, i) + suffix.substring(i + 1),
+						permutations);
+			}
+		}
+	}
+
+	private static void swap(List<Integer> array, int i, int j) {
+		int temp = array.get(i);
+		array.set(i, array.get(j));
+		array.set(j, temp);
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 2, 4));
+		List<List<Integer>> permutations = getPermutations(array);
+		
+		for (List<Integer> permute : permutations)
+			System.out.println(permute);
+		
+		System.out.println("Total number of permutations : " + permutations.size());
+		
+		System.out.println(stringPermutations("abcd"));
+	}
 }
