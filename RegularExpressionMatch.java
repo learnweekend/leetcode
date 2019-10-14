030113ab9caf7c2d0acd7b747b36be54e5312ad8
learnweekend <learnweekend@gmail.com>
2019-10-13 20:32:48 -0700

    Add files via upload

--- RegularExpressionMatch.java
+++ RegularExpressionMatch.java
@@ -1,9 +1,30 @@
+package leetcode;
 
+/**
+ * https://leetcode.com/problems/regular-expression-matching/
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+Note:
+
+    s could be empty and contains only lowercase letters a-z.
+    p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+ *
+ */
 public class RegularExpressionMatch {
-
-	public static void main(String[] args) {
-		
-	}
 
 	public boolean isMatch(String text, String pattern) {
 		if (pattern.isEmpty())
@@ -13,11 +34,9 @@ public class RegularExpressionMatch {
 				(pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
 
 		if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
-			return (isMatch(text, pattern.substring(2)) || 
-					(first_match && isMatch(text.substring(1), pattern)));
+			return (isMatch(text, pattern.substring(2)) || (first_match && isMatch(text.substring(1), pattern)));
 		} else {
 			return first_match && isMatch(text.substring(1), pattern.substring(1));
 		}
 	}
-
 }
